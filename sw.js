56aea937bbf91b282291e152cb9c9031f49be26b
Skarzynski76 <grzegorzskarzynski@icloud.com>
2026-08-17 13:38:27 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'jwstudy-v241s';
+const CACHE = 'jwstudy-v244s';
 /* Rdzeń: dokument + wszystkie moduły CSS i JS — bez nich aplikacja nie ruszy offline. */
 const CORE = [
   './', './index.html'
