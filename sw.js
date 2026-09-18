541da02384b648b500da7cb13c7d6ca95301f6fd
Skarzynski76 <grzegorzskarzynski@icloud.com>
2026-09-18 18:03:11 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Osobny cache dla każdej instalacji (np. dwóch projektów GitHub Pages).
    Starszych, wspólnych cache nie usuwamy: mogą należeć do innej instalacji. */
 const CACHE_PREFIX = 'jwstudy-' + encodeURIComponent(self.registration.scope) + '-';
-const CACHE = CACHE_PREFIX + 'v342';
+const CACHE = CACHE_PREFIX + 'v344';
 const CORE = ['./', './index.html'];
 const EXTRA = [
   './search-worker.js',
